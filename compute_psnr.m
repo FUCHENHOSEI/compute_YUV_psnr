7b7ccca009095fbcb8238f020c0419df9b8b6adb
FUCHENHOSEI <105967475+FUCHENHOSEI@users.noreply.github.com>
2023-09-16 15:13:41 +0900

    Update compute_psnr.m

--- compute_psnr.m
+++ compute_psnr.m
@@ -1,26 +1,63 @@
-function psnr_value = compute_psnr(png1_path, png2_path)
-    % Load PNG images
-    img1 = imread(png1_path);
-    img2 = imread(png2_path);
+save_psnr_to_txt1('BasketballDrill_832x480_50.yuv', 'output_420.yuv', 832, 480, 5);
 
-    % Ensure the images have the same size
-    if size(img1) ~= size(img2)
-        error('The two images have different sizes.');
+function save_psnr_to_txt1(yuvFile1, yuvFile2, width, height, numFrames)
+    % Open the YUV files
+    fid1 = fopen(yuvFile1, 'r');
+    fid2 = fopen(yuvFile2, 'r');
+
+    psnr_Y_vals = zeros(1, numFrames);
+    psnr_U_vals = zeros(1, numFrames);
+    psnr_V_vals = zeros(1, numFrames);
+    psnr_total_vals = zeros(1, numFrames);
+
+    for i = 1:numFrames
+        % Read Y channel
+        Y1 = fread(fid1, [width, height], 'uint8')';
+        Y2 = fread(fid2, [width, height], 'uint8')';
+
+        % Read U channel
+        U1 = fread(fid1, [width/2, height/2], 'uint8')';
+        U2 = fread(fid2, [width/2, height/2], 'uint8')';
+
+        % Read V channel
+        V1 = fread(fid1, [width/2, height/2], 'uint8')';
+        V2 = fread(fid2, [width/2, height/2], 'uint8')';
+
+        % Compute PSNR for each channel
+        psnr_Y_vals(i) = 10 * log10(255^2 / immse(Y1, Y2));
+        psnr_U_vals(i) = 10 * log10(255^2 / immse(U1, U2));
+        psnr_V_vals(i) = 10 * log10(255^2 / immse(V1, V2));
+
+        % Compute total PSNR for the frame
+        totalPixels_Y = width * height;
+        totalPixels_UV = (width/2) * (height/2);
+        mse_total_frame = (immse(Y1, Y2) * totalPixels_Y + immse(U1, U2) * totalPixels_UV + immse(V1, V2) * totalPixels_UV) / (totalPixels_Y + 2 * totalPixels_UV);
+        psnr_total_vals(i) = 10 * log10(255^2 / mse_total_frame);
     end
 
-    % Calculate MSE (Mean Square Error)
-    mse = mean((double(img1) - double(img2)).^2, 'all');
+    fclose(fid1);
+    fclose(fid2);
 
-    % If MSE is zero, PSNR is set to a very high value (infinite theoretically)
-    if mse == 0
-        psnr_value = 100;  % You can also use Inf
-        return;
+    % Save to txt file
+    timestamp = datestr(now,'yyyy_mm_dd_HH_MM_SS');
+    filename = ['PSNR_Comparison_' timestamp '.txt'];
+    fid = fopen(filename, 'w');
+    fprintf(fid, 'Comparison between %s and %s\n\n', yuvFile1, yuvFile2);
+    
+    % Write PSNR values for each frame
+    for i = 1:numFrames
+        fprintf(fid, 'Frame %d:\n', i);
+        fprintf(fid, 'PSNR Y: %f\n', psnr_Y_vals(i));
+        fprintf(fid, 'PSNR U: %f\n', psnr_U_vals(i));
+        fprintf(fid, 'PSNR V: %f\n', psnr_V_vals(i));
+        fprintf(fid, 'Total PSNR: %f\n\n', psnr_total_vals(i));
     end
 
-    % Compute PSNR
-    max_pixel_value = 255.0;  % for 8-bit PNG images
-    psnr_value = 20 * log10(max_pixel_value / sqrt(mse));
+    % Write average PSNR values
+    fprintf(fid, '-------------------------------------\n');
+    fprintf(fid, 'Average PSNR Y: %f\n', mean(psnr_Y_vals));
+    fprintf(fid, 'Average PSNR U: %f\n', mean(psnr_U_vals));
+    fprintf(fid, 'Average PSNR V: %f\n', mean(psnr_V_vals));
+    fprintf(fid, 'Average Total PSNR: %f\n', mean(psnr_total_vals));
+    fclose(fid);
 end
-
-% Example usage:
-
